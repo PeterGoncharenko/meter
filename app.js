155fc64d9c86bd4c76359601cbd8f497a0ea8087
PeterGoncharenko <peter.777.goncharenko@gmail.com>
2022-12-06 15:10:05 +0300

    created decrease button

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const me = 0;
+// set initial count
+let count = 0;
+
+// select balue and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+      count--;
+    }
+    value.textContent = count;
+  });
+});
